5eccf9b23f5ae7b329703512046312623a69b318
unameit10000000 <free4ms@gmail.com>
2025-04-19 01:56:34 +0200

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "energiekijker.nl",
+  description: "bespaar energie met energiekijker.nl",
 };
 
 export default function RootLayout({
